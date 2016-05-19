19121e62590389cda96e0d7217a4ae3dc460df1e
Nitin Shetty <nitin1343@gmail.com>
2016-05-19 16:47:07 -0500

    Move components into a folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 console.log('In webpack config');
  
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
