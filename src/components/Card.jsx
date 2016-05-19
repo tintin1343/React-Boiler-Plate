018f6cdc961daf3e906e5a75a699e2eb19d48d33
Nitin Shetty <nitin1343@gmail.com>
2016-05-19 18:30:06 -0500

    Add inline styles to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 
 class Card extends React.Component {
 	render () {
+		const styles = {
+			image: {
+				width: '100%',
+				height: 200,
+				background: 'url('+ this.props.imgUrl + ') center/cover',
+			},
+			title: {
+				color: 'yellow',
+			}
+		};
+
+
 		return (
 		<div>
-			<div style={{background: 'url('+ this.props.imgUrl + ') center/cover'}}>
-				<h2>{this.props.title}</h2>
+			<div style={styles.image}>
+				<h2 style={styles.title}>{this.props.title}</h2>
 			</div>
 			<div>{this.props.msg}</div>
 		</div>);
